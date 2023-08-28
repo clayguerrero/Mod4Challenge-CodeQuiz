59bb00e860798e213d38aed260abca9031cbdb30
clayg <claytondguerrero@gmail.com>
2023-08-28 15:14:06 -0500

    added query selectors for all containers/ buttons

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,9 @@
+let app = document.querySelector(".app")
+let quiz = document.querySelector(".quiz")
+let question = document.querySelector(".question")
+let answers = document.querySelector(".answers")
+let choice = document.querySelector(".answerChoice")
+let nextContainer = document.querySelector(".nextContainer")
+let nextButton = document.querySelector(".next")
+
 
