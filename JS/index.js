9f6b0789964db9bc90c0bcdcf5d620b2bc923ae1
clayg <claytondguerrero@gmail.com>
2023-08-28 15:19:15 -0500

    changed choice to querySelectorALl

--- JS/index.js
+++ JS/index.js
@@ -2,8 +2,10 @@ let app = document.querySelector(".app")
 let quiz = document.querySelector(".quiz")
 let question = document.querySelector(".question")
 let answers = document.querySelector(".answers")
-let choice = document.querySelector(".answerChoice")
+let choice = document.querySelectorAll(".answerChoice")
 let nextContainer = document.querySelector(".nextContainer")
 let nextButton = document.querySelector(".next")
 
-
+choice.forEach((option) => {
+    option.style.backgroundColor = 'red'
+});
