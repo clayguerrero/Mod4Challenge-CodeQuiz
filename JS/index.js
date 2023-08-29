b16f801c701f697aa01b7f0f9edd22c4ccd107b5
clayg <claytondguerrero@gmail.com>
2023-08-29 15:38:27 -0500

    added score counter, nextQuestion func, resetAnswers func and a few more question placeholders

--- JS/index.js
+++ JS/index.js
@@ -5,6 +5,9 @@ let answers = document.querySelector(".answers");
 let choice = document.querySelectorAll(".answerChoice");
 let nextContainer = document.querySelector(".nextContainer");
 let nextButton = document.querySelector(".next");
+let btn = document.createElement("button");
+let scoreNum = document.querySelector('.score')
+let numCont = document.querySelector('.numContainer')
 
 let curr = 0;
 let score = 0;
@@ -31,12 +34,40 @@ const questions = [
       },
     ],
   },
+  {
+    question: "question2 ",
+    answers: [
+      { text: "a", correct: true },
+      { text: "b", correct: false },
+      { text: "c", correct: false },
+      { text: "d", correct: false },
+    ],
+  },
+  {
+    question: "question3 ",
+    answers: [
+      { text: "a", correct: false },
+      { text: "b", correct: false },
+      { text: "c", correct: true },
+      { text: "d", correct: false },
+    ],
+  },
+  {
+    question: "question4 ",
+    answers: [
+      { text: "a", correct: true },
+      { text: "b", correct: true },
+      { text: "c", correct: false },
+      { text: "d", correct: false },
+    ],
+  },
 ];
 
 function startQuiz() {
   curr = 0;
   score = 0;
 
+  // nextButton.style.display = "none";
   displayQuestion();
 }
 
@@ -45,15 +76,57 @@ function displayQuestion() {
   // console.log(currentQuestion)
   let questionNum = curr + 1;
   // console.log('question number ', questionNum)
+  nextButton.style.display = "none";
   question.textContent = `${questionNum}) ${currentQuestion} `;
 
   // console.log(questions[curr].answers)
-    questions[curr].answers.forEach((answer) => {
-        let btn = document.createElement('button')
-        btn.textContent = answer.text
-        btn.classList.add("answerChoice")
-        answers.appendChild(btn)
+  questions[curr].answers.forEach((answer) => {
+    btn = document.createElement("button");
+    btn.textContent = answer.text;
+    btn.classList.add("answerChoice");
+    answers.appendChild(btn);
+
+    btn.dataset.correct = answer.correct;
+    // console.log(btn, btn.dataset.correct);
+    // console.log(btn.classList)
+    btn.addEventListener("click", select);
   });
+}
+
+function select(e) {
+  let selected = e.target;
+  // scoreNum.innerHTML = score
+
+  if (selected.dataset.correct === "true") {
+    // console.log("u got it");
+    score += 25;
+    scoreNum.textContent = score
+    selected.style.backgroundColor = "green";
+    nextButton.style.justifyContent = "center";
+    nextButton.style.alignItems = "center";
+    nextButton.style.display = "flex";
+  } else if (selected.dataset.correct === "false") {
+    // console.log("nope");
+    score -= 5;
+    scoreNum.textContent = score
+    selected.style.backgroundColor = "red";
+  }
+
+  nextButton.addEventListener("click", nextQuestion);
+}
+
+function nextQuestion() {
+  // console.log(questions[curr]);
+  if (curr < questions.length) {
+    resetAnswers();
+    displayQuestion((curr ++));
+  } else startQuiz();
+}
+
+function resetAnswers() {
+  while (answers.firstChild) {
+    answers.removeChild(answers.firstChild)
+  }
 }
 
 startQuiz();
